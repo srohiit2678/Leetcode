d8c589d3e17fe958e5f59266c7589ef8f4c2e09c
Rohit Sikhwal <rohitsikhwal99@gmail.com>
2024-09-20 01:08:54 +0530

    Create LeetCode28.java

---
+++ LeetCode28.java
@@ -0,0 +1,20 @@
+
+    //haystack = "leetcode", needle = "leeto" //-1
+    //haystack = "sadbutsad", needle = "sad" // 0
+
+public class LeetCode28{
+  static int strStr(String haystack, String needle){
+   for(int i=0;i<haystack.length()-needle.length()+1;i++){
+    if(haystack.charAt(i)==needle.charAt(0)){
+     if(haystack.substring(i,needle.length()+i).equals(needle)){
+      return i;
+       }
+     }
+   } 
+   return -1;   
+  }
+  public static void main(String[] Args){ 
+      System.out.println(strStr( "mississippi","issip"));
+  }
+    } 
+    
