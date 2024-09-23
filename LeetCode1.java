8cd42d07891631a09e4446bc0c010287542006df
Rohit Sikhwal <rohitsikhwal99@gmail.com>
2024-09-23 10:55:08 +0530

    Create LeetCode1.java

---
+++ LeetCode1.java
@@ -0,0 +1,39 @@
+/*
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+ 
+*/
+
+public class LeetCode1 {
+    public static int[] twoSum(int[] nums, int target) {
+        int []res =new int[2];
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]+nums[j]==target){
+                    res[0]=i;   
+                    res[1]=j;
+                    return res;
+                }
+            }
+        }
+        throw new IllegalArgumentException("");
+    } 
+       public static void main(String[] args) {
+        int nums[] = {2,7,11,15}, target = 18;
+        nums =twoSum(nums,target);
+        for (int i : nums) {
+        System.out.print(" "+i);
+        }
+       } 
+}
