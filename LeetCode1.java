f34c52e76996b54548602279921f6b8c22b088ff
srohiit2678 <rohitsikhwal99@gmail.com>
2024-09-30 10:59:52 +0530

    Update LeetCode1.java

--- LeetCode1.java
+++ LeetCode1.java
@@ -17,23 +17,24 @@ Output: [0,1]
 
 public class LeetCode1 {
     public static int[] twoSum(int[] nums, int target) {
-        int []res =new int[2];
-        for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[i]+nums[j]==target){
-                    res[0]=i;   
-                    res[1]=j;
+        int[] res = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    res[0] = i;
+                    res[1] = j;
                     return res;
                 }
             }
         }
         throw new IllegalArgumentException("");
-    } 
-       public static void main(String[] args) {
-        int nums[] = {2,7,11,15}, target = 18;
-        nums =twoSum(nums,target);
+    }
+
+    public static void main(String[] args) {
+        int nums[] = { 2, 7, 11, 15 }, target = 18;
+        nums = twoSum(nums, target);
         for (int i : nums) {
-        System.out.print(" "+i);
+            System.out.print(" " + i);
         }
-       } 
+    }
 }
