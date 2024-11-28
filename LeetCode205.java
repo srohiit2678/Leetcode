d391d339964f1dfc8662e73fb3606e558b911b03
srohiit2678 <rohitsikhwal99@gmail.com>
2024-11-28 10:42:59 +0530

    Create LeetCode205.java

---
+++ LeetCode205.java
@@ -0,0 +1,54 @@
+/*
+ 
+Given two strings s and t, determine if they are isomorphic.
+Two strings s and t are isomorphic if the characters in s can
+be replaced to get t.
+All occurrences of a character must be replaced with another 
+character while preserving the order of characters. No two 
+characters may map to the same character, but a character may 
+map to itself.
+
+ 
+
+Example 1:
+Input: s = "egg", t = "add"
+Output: true
+Explanation:
+The strings s and t can be made identical by:
+Mapping 'e' to 'a'.
+Mapping 'g' to 'd'.
+
+Example 2:
+Input: s = "foo", t = "bar"
+Output: false
+Explanation:
+The strings s and t can not be made identical as 'o' needs to be 
+mapped to both 'a' and 'r'.
+
+Example 3:
+Input: s = "paper", t = "title"
+Output: true
+*/
+import java.util.HashMap;;
+public class LeetCode205 {
+  static boolean isIsomorphic(String s, String t) {
+   HashMap<Character,Character> contain = new HashMap<Character,Character>();
+  for(int i=0;i<s.length();i++){
+    if(contain.containsKey(s.charAt(i))){
+      if(!(contain.get(s.charAt(i)).equals(t.charAt(i)))){
+        return false;
+      }
+    }
+    else{
+        if(contain.containsValue(t.charAt(i))){
+          return false;
+        }
+      contain.put(s.charAt(i),t.charAt(i));
+    }
+  }
+    return true;     
+  }
+  public static void main(String[] args) {
+    System.out.println(isIsomorphic("paper", "title"));
+  }
+}
