e472f02ebb887c17505e7cdae438cb046ba3fb31
srohiit2678 <rohitsikhwal99@gmail.com>
2024-12-02 23:11:50 +0530

    Update LeetCode8.java

--- LeetCode8.java
+++ LeetCode8.java
@@ -128,17 +128,27 @@ public class LeetCode8 {
             if(s.charAt(i)=='-')neg = true;
             i++;
         }
-        while( i<len && (s.charAt(i)>=48 && s.charAt(i)<=57)){ 
-            int val = s.charAt(i)-48;
-            System.out.println(res==Integer.MAX_VALUE/10 && (s.charAt(i)>Integer.MAX_VALUE%10));
-        if(res > Integer.MAX_VALUE/10 ||( res == Integer.MAX_VALUE/10 && s.charAt(i)>Integer.MAX_VALUE%10 )){
-            if(neg ||res*10==Integer.MAX_VALUE){
-                return Integer.MIN_VALUE;
+        // while( i<len && (s.charAt(i)>=48 && s.charAt(i)<=57)){ 
+        //     int val = s.charAt(i)-48;
+        //     System.out.println(res==Integer.MAX_VALUE/10 && (s.charAt(i)>Integer.MAX_VALUE%10));
+        // if(res > Integer.MAX_VALUE/10 ||( res == Integer.MAX_VALUE/10 && s.charAt(i)>Integer.MAX_VALUE%10 )){
+        //     if(neg ||res*10==Integer.MAX_VALUE){
+        //         return Integer.MIN_VALUE;
+        //     }
+        //     return Integer.MAX_VALUE;
+        // }
+        // res = res*10 + val; 
+        // i++;
+        // }
+        while (i < s.length() && Character.isDigit(s.charAt(i))) {
+            int digit = s.charAt(i) - '0';
+
+            if (res > Integer.MAX_VALUE / 10 || (res == Integer.MAX_VALUE / 10 && digit > Integer.MAX_VALUE % 10)) {
+                return neg ? Integer.MIN_VALUE : Integer.MAX_VALUE;
             }
-            return Integer.MAX_VALUE;
-        }
-        res = res*10 + val; 
-        i++;
+
+            res = res * 10 + digit;
+            i++;
         }
         if(neg)res=-res;
 
@@ -147,8 +157,8 @@ public class LeetCode8 {
 
 
 public static void main(String[] args) {
-//   System.out.println(myAtoi("-91283472332")); 
-  System.out.println(myAtoi("2147483646"));
+  System.out.println(myAtoi("-91283472332")); 
+//   System.out.println(myAtoi("2147483647"));
 
 }
 
